47c4e77ec92457d220510313a68dea31ca39aee6
LeoMuleya <168299696+LeoMuleya@users.noreply.github.com>
2024-07-26 16:55:08 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -26,29 +26,33 @@ function closeDonationModal() {
   }
 /*---------------------------Our Slidshow -------------------*/
 
-let slideIndex = 1;
-showSlides(slideIndex);
+$(document).ready(function() {
+  $('.slider').slick({
+      dots: true,  // Enable dots for navigation
+      arrows: false, // Hide arrows
+      infinite: true,
+      speed: 800,
+      fade: true,
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 3000,
+      pauseOnHover: false
+  });
+});
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+//--------------------------- Magazine 
+document.addEventListener('DOMContentLoaded', () => {
+  const stars = document.querySelectorAll('.star');
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
+  stars.forEach(star => {
+      star.addEventListener('click', () => {
+          const value = star.getAttribute('data-value');
+          stars.forEach(s => {
+              s.classList.remove('selected');
+              if (s.getAttribute('data-value') <= value) {
+                  s.classList.add('selected');
+              }
+          });
+      });
+  });
+});
